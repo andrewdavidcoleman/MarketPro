e54855adc96d35d7fe4015c44a2853566053eb3b
Andrew Coleman <Andrew@lawn-128-61-127-169.lawn.gatech.edu>
2017-08-24 20:03:48 -0400

    deploying heroku

--- controllers/trendController.js
+++ controllers/trendController.js
@@ -3,7 +3,7 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (trends.js) to use its database functions.
-var trend = require("../models/trends.js");
+var trend = require("../trends.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
