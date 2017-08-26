fdb5fc40d02250f202ddef964eab30a70d5cc262
R3dWulf <theakaiokami@gmail.com>
2017-08-26 11:15:14 -0400

    m

--- public/assets/javascript/script.js
+++ public/assets/javascript/script.js
@@ -2,56 +2,15 @@ $(document).ready(function(){
 
   var total = 0;
 
-// var mysql = require("mysql");
-
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "root",
-//   database: "marketpro_db"
-// });
-
-// connection.connect(function(err) {
-//   if (err) throw err;
-//   console.log("connected as id " + connection.threadId + "\n");
-//   createProduct();
-// });
-
 
 // var queryURL = "https://market-pro-2017.herokuapp.com/api/sales";
 var queryURL = "http://localhost:3000/api/sales";
 
 
-// $.ajax({
-//   url: queryURL,
-//   type: 'GET',
-//   crossDomain: true,
-//   dataType: 'json',
-//   error: function(error) {
-//      console.log('***error***:' + error);
-//    }
-// })
-// .done(function(response) {
-//   // console.log(response);
-// //   $(".metric1").html("<p>" + response[0].metric1 + "</p>");
-// //   $(".metric2").html(response[0].metric2)
-// //   $(".metric3").html(response[0].metric3)
-//       Object.keys(response).forEach(function(key) {
-//           var saleObj = response[key]; 
-//           console.log("^^^^^^"+saleObj["salesperson"]);
-//           $("#running_employee_total").append("<p>"+saleObj["salesperson"]+"</p>");
-//           // $("running_employee_total").html("<p>" + saleObj[key]+ "</p>");
-//           // no data is going to metric columns, test to see if input boxes are submitting a value
-//           //console.log("@@@@@" + saleObj);
-//       });
-// });
-
-
-
     $("#submitSales").on("submit", function() {
 
       event.preventDefault();
+
       var employeeNameInput = $(".name-input").val().trim();
       //metric colums cannot be emplty and must be an integer or sequelize error will be thrown
       // html required checks for empty
@@ -69,7 +28,7 @@ var queryURL = "http://localhost:3000/api/sales";
          salesperson: $(".name-input").val().trim(),
          metric1: $(".metric1-input").val().trim(),
          metric2: $(".metric2-input").val().trim(),
-         metric3: $(".metric3-input").val().trim()
+         metric3: $(".metric3-input").val().trim() 
         };
 
 
@@ -117,8 +76,8 @@ var queryURL = "http://localhost:3000/api/sales";
 
              }
           });
-                     $("#running_employee_total").append("<p>"+total+"</p>");
-                     //console.log(total);
+          $("#running_employee_total").append("<p>"+total+"</p>");
+          //console.log(total);
 
 
         //end ajax loop
@@ -129,22 +88,9 @@ var queryURL = "http://localhost:3000/api/sales";
       }
       //Call the newSale Function
       newSale();
-      //End isNaN
-      
-
-      // function grabSales(){
-      //   var query = connection.query(
-      //     "SELECT SUM(metric1 + metric2 + metric3) FROM sales WHERE salesperson = " + employeeNameInput+,
-      //     function(err, res) {
-      //     console.log(res);
-      //   }
-      // );
-
-        // grabSales();
       }
     // End submit
     });
-
 // bottom of document ready
 });
 
