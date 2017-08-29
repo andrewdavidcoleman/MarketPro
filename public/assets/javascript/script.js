fce3b223643fe9566ba2281e38646c21cf522a11
Andrew Coleman <Andrew@lawn-128-61-127-169.lawn.gatech.edu>
2017-08-28 22:20:56 -0400

    commit

--- public/assets/javascript/script.js
+++ public/assets/javascript/script.js
@@ -3,59 +3,38 @@ $(document).ready(function(){
   var total = 0;
 
 
-  // var queryURL = "https://market-pro-2017.herokuapp.com/api/sales";
-  var queryURL = "http://localhost:3000/api/sales";
+// var queryURL = "https://market-pro-2017.herokuapp.com/api/sales";
+var queryURL = "http://localhost:3000/api/sales";
 
-  $("#submit-person").on("click", function() {
+ $("#submitSales").on("submit", function() {
 
-    function newPerson() {
+
+  event.preventDefault()
+
 
       event.preventDefault();
 
-      var person = {
-        firstName: $(".first-name-input").val().trim(),
-        lastName: $(".last-name-input").val().trim()
-      }
+      var employeeNameInput = $(".name-input").val().trim();
+      //metric colums cannot be emplty and must be an integer or sequelize error will be thrown
+      // html required checks for empty
+      var a = $(".metric1-input").val().trim();
+      var b = $(".metric2-input").val().trim();
+      var c = $(".metric2-input").val().trim();
 
-      $.ajax({
-          url: 'http://localhost:3000/api/people',
-          type: 'post',
-          dataType: 'json',
-          data: person,
-          success: function (data) {
-            console.log(data);
-          },
-          error: function(error) {
-            console.log(error);
-          }
-      });
-      // End newPerson function
-    };
-
-    newPerson();
-
-  });
-
-  $("#submit").on("click", function() {
-
-    event.preventDefault();
-
-    var a = $(".metric1-input").val().trim();
-    var b = $(".metric2-input").val().trim();
-    var c = $(".metric3-input").val().trim();
-
-    if (isNaN(a) || isNaN(b) || isNaN(c) ){
-      return false;
-    } else {
+      if (isNaN(a) || isNaN(b) || isNaN(c) ){
+        return false;
+      } else {
 
       function newSale() {
 
         var sale = {
+         salesperson: $(".name-input").val().trim(),
          metric1: $(".metric1-input").val().trim(),
          metric2: $(".metric2-input").val().trim(),
-         metric3: $(".metric3-input").val().trim(),
-         PersonId: $(".emp-id").val().trim()
+         metric3: $(".metric3-input").val().trim()
         };
+
+
 
         $.ajax({
             url: 'http://localhost:3000/api/sales',
@@ -63,19 +42,57 @@ $(document).ready(function(){
             dataType: 'json',
             data: sale,
             success: function (data) {
-              console.log(data);
+                //console.log("!!!!!!!"+data);;
             },
-            error: function(error) {
-              console.log(error);
-            }
+
         });
 
-        // End newSale function
+
+        $.ajax({
+          url: queryURL,
+          type: 'GET',
+          crossDomain: true,
+          dataType: 'json',
+          error: function(error) {
+             console.log('***error***:' + error);
+           }
+        })
+        .done(function(response) {
+          //console.log(response);
+          response.forEach(function(element){
+            //console.log(element);
+             for(key in element){
+              //console.log(key);
+              //console.log(element[key]);
+              // var isTom = element[key].toString().indexOf("Tom Tom");
+              var isTom = element["salesperson"];
+              //console.log(isTom);
+              if(isTom == employeeNameInput ) {
+                //console.log(isTom);
+                //adding the objects
+                var metricsAddedValue = element["metric1"]+ element["metric2"]+element["metric3"];
+                //combine
+                //console.log(metricsAddedValue);
+                total += parseInt(metricsAddedValue);
+              }
+
+
+             }
+          });
+          $("#running_employee_total").append("<p>"+total+"</p>");
+          //console.log(total);
+
+
+        //end ajax loop
+        });
+
+
+      // End newSale function
       }
       //Call the newSale Function
       newSale();
-    }
+      }
     // End submit
-  });
-  // bottom of document ready
+    });
+// bottom of document ready
 });
