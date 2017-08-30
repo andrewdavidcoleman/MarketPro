e18b1847e9ee57bdfa57bda78fde6c37bc0b3c18
R3dWulf <theakaiokami@gmail.com>
2017-08-29 20:50:49 -0400

    two pages

--- public/assets/javascript/login.js
+++ public/assets/javascript/login.js
@@ -15,59 +15,12 @@ $(document).ready(function() {
      		console.log("<><><><> inputs are submitting blank values");
      	}
 
-     	// if(employeeUsernameInput == "loginfailed"){
-     	// 	people_id = 1;
-     	// }
-
-
-   //   	$.ajax({
-			// url: queryURL, 
-			// dataType: 'json',
-			// type: 'post',
-	  // 		data: {
-	  // 		firstName: employeeFirstNameInput,
-   //   			lastName: employeeLastNameInput
-	  // 		},
-	  // 		success: function(data){
-	  // 			$.ajax({
-	  // 				url: queryURL,
-   //                  	type: 'GET',
-   //                  	crossDomain: true,
-   //                  	dataType: 'json',
-   //                  	error: function(error) {
-   //                    	//console.log('***error***:' + error);
-   //                  	}
-	  // 			}).done(function(response){
-	  // 				//console.log(response); 
-	  // 				response.forEach(function(response){
-		 //  				for(key in response){
-		 //  					//console.log(response);
-		 //  				 	var isFirstName = response["firstName"];
-		 //  				 	var isLastName  = response["lastName"];
-		 //  				 	if(employeeFirstNameInput == isFirstName && employeeFirstNameInput == isLastName){
-		 //  				 		people_id = response["id"]; 
-		 //  				 		// console.log("<><><>: "+ isFirstName + " " + isLastName);
-		 //  				 		// console.log(response["id"]);
-		 //  				 	} 
-		 //  				}
-		 //  			});
-
-	  // 				window.location.replace("/dashboard?people_id=" + people_id);
-	  // 			//End done
-	  // 			});
-	  // 		//End success
-	  // 		}
-
 
 	  	$.ajax({
 	  		url: queryURL,
 	  		dataType: "json",
 	  		type: "get",
 	  		crossDomain: true,
-	  		// data: {
-	  		// 	userName: employeeUsernameInput,
-	  		// 	password: employeePasswordInput
-	  		// },
 	  		error: function(error){
 	  			console.log(error);
 	  			}
